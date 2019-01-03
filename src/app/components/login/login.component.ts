0873b935dd10646c65e98f3fa59f3700e802abd7
Alexandre Bacellar <alexandre.bacellar@rededor.com.br>
2019-01-03 15:46:27 -0200

    Implement login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from 'src/app/services/http.service';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-login',
@@ -10,16 +12,46 @@ export class LoginComponent implements OnInit {
 
   message: string = 'wait for it...';
 
-  question = 'login';
-
   route = '/';
 
+  passWarningState = 'hidden';
+
+  loginForm = this.fb.group({
+    username: ['', Validators.required],
+    password: ['']
+  });
+
   constructor(
-    private httpService: HttpService
+    private httpService: HttpService,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit() {
     this.httpService.getHelloWorld().subscribe(msg => this.message = msg);
+    this.loginForm.get('password').valueChanges.subscribe(pass => {
+      if(pass) this.hideWarning();
+    });
+  }
+
+  onSubmit() {
+    console.log('submit', this.loginForm.value);
+  }
+
+  showWarning() {
+    if(this.passWarningState != 'hidden') return;
+    this.passWarningState = 'animating';
+    $('#password-warning').css('height', '10px').show().animate({height: "150px"}, 600, 'swing', () => {
+      this.passWarningState = 'shown';
+    });
+  }
+
+  hideWarning() {
+    if(this.passWarningState != 'shown') return;
+    this.passWarningState = 'animating';
+    $('#password-warning').css('height', '150px').animate({height: "10px"}, 600, 'swing', () => {
+      $('#password-warning').hide();
+      this.passWarningState = 'hidden';
+    });
   }
 
 }
